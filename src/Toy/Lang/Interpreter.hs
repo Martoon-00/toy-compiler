109f7b8be23bca2c3daa8ae707d9f25f3b76033d
Konstantin Ivanov <Martoon_00@mail.ru>
2017-03-17 23:44:49 +0300

    Cleanup, fix comment

--- src/Toy/Lang/Interpreter.hs
+++ src/Toy/Lang/Interpreter.hs
@@ -10,14 +10,14 @@ import qualified Data.Map      as M
 import           Toy.Exp       (eval)
 import           Toy.Lang.Data (Exec, ExecState (..), Stmt (..), withStmt)
 
--- | Proceed in given program state, halting at `SkipS` operation.
+-- | Proceed in given program state, halting at `Skip` operation.
 execute :: ExecState -> Exec
 execute (ExecState is os vars stmt@(var := expr)) = do
     value <- withStmt stmt $ eval expr vars
     return $ ExecState is os (M.insert var value vars) Skip
 
 execute (ExecState (i:is) os vars (Read var)) =
-    Right $ ExecState is os (M.insert var i vars) Skip
+    return $ ExecState is os (M.insert var i vars) Skip
 execute (ExecState [] _ _ stmt@(Read _)) =
     Left (stmt, "Input unavailable")
 
@@ -27,7 +27,6 @@ execute (ExecState is os vars stmt@(Write expr)) = do
 
 execute (ExecState is os vars stmt@(If cond stmt0 stmt1)) = do
     cond' <- withStmt stmt $ eval cond vars
-    -- TODO: scope?
     execute . ExecState is os vars $
         if cond' /= 0 then stmt0 else stmt1
 
@@ -37,7 +36,7 @@ execute (ExecState is os vars while@(While cond body)) =
 execute (ExecState is os vars (Seq stmt0 stmt1)) = do
     ExecState is' os' vars' end <- execute (ExecState is os vars stmt0)
     case end of
-        Skip -> execute (ExecState is' os' vars' stmt1)
+        Skip -> execute $ ExecState is' os' vars' stmt1
         _    -> error "execute: unexpected end operation"
 
 execute exec@(ExecState _ _ _ Skip) =
