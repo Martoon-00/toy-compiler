148a979b7ef345b2aba24c732c01254d2ff2ec4f
Martoon <martoon.hsk@gmail.com>
2017-05-28 16:34:15 +0300

    Correctly process errors in SM interpreter

--- src/Toy/SM/Interpreter.hs
+++ src/Toy/SM/Interpreter.hs
@@ -1,45 +1,38 @@
 {-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ViewPatterns  #-}
 
 module Toy.SM.Interpreter
     ( execute
     ) where
 
-import           Control.Lens               (at, use, (%=), (+=), (.=), (<<%=), (?=),
-                                             (^.))
-import           Control.Monad              (forever, mzero, replicateM, void, when)
-import           Control.Monad.Error.Class  (throwError)
-import           Control.Monad.Morph        (hoist)
-import           Control.Monad.State.Strict (StateT)
-import           Control.Monad.Trans        (lift)
-import           Control.Monad.Trans.Either (EitherT)
-import           Data.Conduit               (await, yield)
-import           Data.Conduit.Lift          (evalStateC, execStateC, runMaybeC)
-import           Data.Default               (def)
-import           Data.Functor               (($>))
-import qualified Data.Map                   as M
-import           Data.Maybe                 (fromMaybe)
-import qualified Data.Vector                as V
-import           Formatting                 (formatToString, int, shown, string, (%))
-import           Universum                  (type ($), whenNothing)
+import           Control.Lens              (at, use, (%=), (+=), (.=), (<<%=), (?=), (^.))
+import           Control.Monad             (forever, mzero, replicateM, void, when)
+import           Control.Monad.Error.Class (MonadError (..))
+import           Control.Monad.Morph       (hoist)
+import           Control.Monad.Trans       (lift)
+import           Data.Conduit              (await, yield)
+import           Data.Conduit.Lift         (evalStateC, execStateC, runMaybeC)
+import           Data.Default              (def)
+import           Data.Functor              (($>))
+import qualified Data.Map                  as M
+import qualified Data.Vector               as V
+import           Formatting                (formatToString, int, shown, string, (%))
+import           Universum                 (whenNothing)
 
-import           Toy.Base                   (Exec, ExecInOut, FunSign (..))
-import           Toy.Exp                    (arithspoon, binOp)
-import           Toy.SM.Data                (ExecState (..), IP, Inst (..), Insts,
-                                             LabelId (..), esIp, esLocals, esStack,
-                                             initFunName)
-
-type ExecProcess m = ExecInOut $ StateT ExecState $ EitherT String m
+import           Toy.Base                  (Exec, FunSign (..))
+import           Toy.Exp                   (arithspoon, binOp)
+import           Toy.SM.Data               (ExecState (..), IP, Inst (..), Insts,
+                                            LabelId (..), esIp, esLocals, esStack,
+                                            initFunName)
 
 execute :: Monad m => Insts -> Exec m ()
-execute insts = evalStateC def{ _esIp = findEntry insts } $ executeDo insts
-
-executeDo :: Monad m => Insts -> ExecProcess m ()
-executeDo insts = void . runMaybeC . forever $
-    getCurInst >>= step >> esIp += 1
+execute insts = evalStateC def{ _esIp = programEntry } $ executeDo
   where
+    -- executeDo :: Monad m => ExecInOut $ StateT ExecState $ EitherT String m
+    executeDo = void . runMaybeC . forever $
+        getCurInst >>= step >> esIp += 1
+
     getCurInst = do
         i <- use esIp
         insts V.!? i `whenNothing` throwError "To the space and further! >>>"
@@ -57,7 +50,7 @@ executeDo insts = void . runMaybeC . forever $
         Label{}    -> step Nop
         Jmp lid    -> do
             ensureStackSize 0 "jump"
-            esIp .= getLabel (CLabel lid)
+            (esIp .= ) =<< getLabel (CLabel lid)
         JmpIf lid  -> do
             cond <- pop
             when (cond /= 0) $ step (Jmp lid)
@@ -67,15 +60,16 @@ executeDo insts = void . runMaybeC . forever $
             pop >>= yield >> push 0
         Call (FunSign name args) -> do
             stack <- esStack <<%= drop (length args)
+            entry <- getLabel (FLabel name)
             let funExecState = ExecState
                     { _esLocals = M.fromList (zip args stack)
                     , _esStack  = []
-                    , _esIp     = getLabel (FLabel name)
+                    , _esIp     = entry
                     }
             -- run execution with its own `ExecState`, not allowing it to
             -- infulence on our current state
             funEndExecState <-
-                hoist (lift . lift) $ execStateC funExecState $ executeDo insts
+                hoist (lift . lift) $ execStateC funExecState executeDo
 
             case _esStack funEndExecState of
                 [x]   -> esStack %= (x:)
@@ -95,16 +89,16 @@ executeDo insts = void . runMaybeC . forever $
                             int%" instead, reason: "%string)
                            (length st) size reason
     badStackAtFunEnd = formatToString ("Not 1 argument on function end: "%shown)
+    getLabel :: MonadError String m => LabelId -> m IP
     getLabel = buildLabelsMap insts
+    programEntry =
+        either (error "No entry point exists") id $
+        getLabel (FLabel initFunName)
 
-buildLabelsMap :: Insts -> LabelId -> IP
+buildLabelsMap :: MonadError String m => Insts -> LabelId -> m IP
 buildLabelsMap (V.toList -> insts) =
     let addLabel (idx, Label li) = M.insert li idx
         addLabel _               = id
         labelsMap = foldr addLabel M.empty $ zip [0..] insts
-    in  \labelId -> fromMaybe (error $ "No label " ++ show labelId) $
-                    labelsMap ^. at labelId
-
--- TODO: don't recalculate labels map
-findEntry :: Insts -> IP
-findEntry = (`buildLabelsMap` FLabel initFunName)
+    in  \labelId -> (labelsMap ^. at labelId) `whenNothing`
+                        throwError ("No label " ++ show labelId)
